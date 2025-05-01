88feda4ec758001f48e4d15c142191521e5e6770
karamjeet2k <karamjeetkaur2k@gmail.com>
2025-05-01 13:41:14 -0700

    basic game setting and variables

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,30 @@
 package application;
 
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-change new change
+import javafx.application.Application;
+public class Main extends Application {
+    //    basic add Varibles
+    static int speed = 5;
+    static int foodcolor = 0;
+    static int width = 25;
+    static int height = 25;
+    static int foodX = 0;
+    static int foodY = 0;
+    static int cornersize = 30;
+    static List<Corner> snake = new ArrayList<>();
+
+
+    public static class Corner {
+        int x;
+        int y;
+
+        public Corner(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+}
